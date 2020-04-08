f6319c6885327f80510b36b0bfb2839da5f3be56
Jean Lamarre <jonny.lamarre@hotmail.com>
2020-04-08 16:26:45 -0400

    Init Commit

---
+++ src/com/DjembeInc/Johnytech6Plugin/JohnytechPlugin.java
@@ -0,0 +1,14 @@
+package com.DjembeInc.Johnytech6Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class JohnytechPlugin  extends JavaPlugin{
+	 // Fired when plugin is first enabled
+    @Override
+    public void onEnable() {
+    }
+    // Fired when plugin is disabled
+    @Override
+    public void onDisable() {
+
+    }
+}
